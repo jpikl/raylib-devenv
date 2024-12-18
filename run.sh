34f96d3ff6763824baf50d9a7ef05cbe64074d1b
Jan Pikl <pikl.jan@gmail.com>
2024-12-18 06:30:09 +0100

    Disable docker run interactive on CI

--- run.sh
+++ run.sh
@@ -7,11 +7,17 @@ SCRIPT_DIR=$(dirname -- "$0")
 # shellcheck source=./config.sh
 . "$SCRIPT_DIR/config.sh"
 
-"$DOCKER" run \
+# Prepend default `docker run` options
+set -- \
     --rm \
-    --tty \
-    --interactive \
     --volume "$PWD:/mnt" \
     --workdir /mnt \
     "$TAG" \
     "$@"
+
+# Enable interactivity only when run from terminal and not on CI
+if [ -t 0 ] && [ -t 1 ] && [ ! "${CI-}" ]; then
+    set -- --tty --interactive "$@"
+fi
+
+echo "$DOCKER" run "$@"
