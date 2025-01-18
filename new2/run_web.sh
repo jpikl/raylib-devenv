057134125715bb286638c9043ac848f01bf17311
Jan Pikl <pikl.jan@gmail.com>
2025-01-18 14:21:22 +0100

    More reliable web run

--- new2/run_web.sh
+++ new2/run_web.sh
@@ -8,20 +8,20 @@ source "$ROOT_DIR/common.sh"
 source "$ROOT_DIR/common_build.sh"
 source "$ROOT_DIR/common_web.sh"
 
-URL=http://127.0.0.1:8000
+PORT=8000
+URL=http://127.0.0.1:$PORT
 
-if [ -x "$(command -v python)" ]; then
-    if [ -x "$(command -v xdg-open)" ]; then
+if [[ -x "$(command -v emrun)" ]]; then
+    emrun --port "$PORT" "$WEB_OUT_DIR"
+elif [[ -x "$(command -v python)" ]]; then
+    if [[ -x "$(command -v xdg-open)" ]]; then
         (sleep 1; xdg-open "$URL") &
     else
-        echo "Open the following URL in your browser"
-        echo
-        echo "    $URL"
-        echo
+        echo "Open the following URL in your browser: $URL"
     fi
-    python -m http.server -b 0.0.0.0 -d "$WEB_OUT_DIR"
+    python -m http.server -b 0.0.0.0 -d "$WEB_OUT_DIR" "$PORT"
+elif [[ -x "$(command -v npx)" ]]; then
+    npx http-server -c-1 -o -p "$PORT" "$WEB_OUT_DIR"
 else
-    echo >&2 "Could not start python http server!"
-    echo >&2
-    echo >&2 "Install python first or use some other command to serve files from '$WEB_OUT_DIR' via http."
+    die "Unable to start HTTP server to serve files from '$WEB_OUT_DIR'"
 fi
