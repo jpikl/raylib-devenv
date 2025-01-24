3d956b5035425c4c873b941a4e258a187b00a2f4
Jan Pikl <pikl.jan@gmail.com>
2025-01-24 02:13:43 +0100

    Add test cases

---
+++ new2/self_test.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -eu
+
+ROOT_DIR=$(dirname "$0")
+
+source "$ROOT_DIR/common.sh"
+
+# ================================================================================
+# Runtime
+# ================================================================================
+
+CASES=()
+RESULTS=()
+
+header() {
+    echo
+    info "================================================================================"
+    info " $1"
+    info "================================================================================"
+    echo
+}
+
+index_to_case_id() {
+    printf "TC-%02d" $(($1+1))
+}
+
+assert() {
+    INDEX=${#CASES[@]}
+    ID=$(index_to_case_id "$INDEX")
+    header "[$ID] $1"
+
+    RESULT=0
+    sh -c "$1" || RESULT=$?
+
+    CASES+=("$1")
+    RESULTS+=("$RESULT")
+}
+
+cd "$ROOT_DIR/example"
+
+# ================================================================================
+# Linux
+# ================================================================================
+
+assert "../build_linux_image.sh"
+assert "../build_linux_dockerized.sh"
+assert "test -x out/linux/app"
+assert "test -f out/linux/assets/coin.wav"
+assert "test -f out/linux/assets/raylib.png"
+
+# ================================================================================
+# Web
+# ================================================================================
+
+assert "../build_web_image.sh"
+assert "../build_web_dockerized.sh"
+assert "test -f out/web/index.data"
+assert "test -f out/web/index.html"
+assert "test -f out/web/index.js"
+assert "test -f out/web/index.wasm"
+
+# ================================================================================
+# Android
+# ================================================================================
+
+assert "../build_android_image.sh"
+assert "../build_android_dockerized.sh"
+
+# ================================================================================
+# Results
+# ================================================================================
+
+header "TEST RESULTS"
+
+FINAL_RESULT=0
+
+for INDEX in "${!CASES[@]}"; do
+    ID=$(index_to_case_id "$INDEX")
+    CASE=${CASES[$INDEX]}
+    RESULT=${RESULTS[$INDEX]}
+
+    if [[ $RESULT -eq 0 ]]; then
+        success "$ID [OK] $CASE"
+    else
+        error "$ID [ERR] $CASE"
+        FINAL_RESULT=1
+    fi
+done
+
+exit $FINAL_RESULT
